b421f90ab1296eff415a40226f0e9c058d5ac694
topseySuave <gabrielsuave17@gmail.com>
2019-01-15 23:02:09 +0100

    Set up Todo application

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './App.css';
+
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        body
+      </div>
+    );
+  }
+}
+
+export default App;
